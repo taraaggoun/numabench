d86b68395ce647605d211468da3136b6a1466749
Jérôme <git@jeromecst.com>
2023-05-24 12:17:23 +0200

    no debug by default

--- numabench.c
+++ numabench.c
@@ -501,7 +501,7 @@ int main(int argc, char *argv[]) {
 		.pages_migration = false,
 		.thread_migration = false,
 		.random_operation = true,
-		.debug = true,
+		.debug = false,
 	};
 
 	parse_args(argc, argv, &config);
